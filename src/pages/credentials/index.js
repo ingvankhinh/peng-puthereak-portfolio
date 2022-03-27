0e2e45df83f19db2d0d59f6f5852170d3310185c
Ing Vankhinh <ingvankhinh45@gmail.com>
2022-03-27 09:55:32 +0700

    Refactor Credential page

--- src/pages/credentials/index.js
+++ src/pages/credentials/index.js
@@ -1,11 +1,10 @@
-import React, { useState, useRef } from "react";
-import { connect } from "react-redux";
+import React from "react";
 
 import Master from "../../layouts/Master";
 import ExperienceDetail from "./ExperienceDetail";
 import ExperienceTable from "./ExperienceTable";
 
-const Credentials = ({ language }) => {
+const Credentials = () => {
     return (
         <Master>
             <div className="w-full h-full px-28 py-20 2xl:px-24 xl:px-20 lg:px-16 md:px-10 md:flex-col sm:px-7">
@@ -16,8 +15,4 @@ const Credentials = ({ language }) => {
     );
 };
 
-const mapStateToProps = (states) => {
-    return { language: states.language };
-};
-
-export default connect(mapStateToProps, null)(Credentials);
+export default Credentials;
